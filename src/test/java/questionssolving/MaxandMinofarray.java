2cb0369214ddc8c9541c319cb1b8ad2fca850ebd
bapparaya <boppamuthuraj78605@gmail.com>
2025-02-23 16:23:21 +0530

    new changes

---
+++ src/test/java/questionssolving/MaxandMinofarray.java
@@ -0,0 +1,15 @@
+package questionssolving;
+
+import java.util.Arrays;
+
+public class MaxandMinofarray {
+
+	public static void main(String[] args) {
+		int[] a = {2,1,5,9,6,3,4};
+		System.out.println(a[0]);
+		Arrays.sort(a);
+		System.out.println(a[0]);
+
+	}
+
+}
